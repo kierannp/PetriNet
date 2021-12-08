7c869ec9d309f9a40f8567c937eb41beb5cfcffa
Kieran Nehil-Puleo <knp@Kierans-MacBook-Pro.local>
2021-12-08 13:28:55 -0600

    JointJS import and default project

--- config/config.default.js
+++ config/config.default.js
@@ -8,6 +8,10 @@ const mongoHost = process.env.MONGO_HOST || '127.0.0.1';
 config.mongo.uri = `mongodb://${mongoHost}:27017/webgme_dcrypps`;
 config.plugin.allowServerExecution = true;
 
+config.seedProjects.defaultProject = 'PetSeed';
+
+config.requirejsPaths['jointjs'] = './node_modules/jointjs/dist/joint.min';
+
 
 validateConfig(config);
 module.exports = config;
